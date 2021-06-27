7605ea3dbdcf584ece73609bbb62a205681059fa
= <=>
2021-06-27 08:43:56 +0700

    debug

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -22,18 +22,18 @@ def arithmetic_arranger(problems, result = False):
         #Standardize data
         while len(split[LEFT_OPERAND]) > len(split[RIGHT_OPERAND]):
             split[RIGHT_OPERAND] = ' ' + split[RIGHT_OPERAND]
-        while len(split[LEFT_OPERAND]) < len(split[RIGHT_OPERAND]):
-            split[LEFT_OPERAND] = ' ' + split[LEFT_OPERAND]
 
         split[RIGHT_OPERAND] = split[OPERATOR] + ' ' + split[RIGHT_OPERAND]
         split.append('-' * len(split[RIGHT_OPERAND]))
-        
+        while len(split[LEFT_OPERAND]) < len(split[RIGHT_OPERAND]):
+            split[LEFT_OPERAND] = ' ' + split[LEFT_OPERAND]
+
         if result:
             caculation = 0
             if split[OPERATOR] == '-':
-                caculation = split[LEFT_OPERAND] - split[RIGHT_OPERAND]
+                caculation = int(split[LEFT_OPERAND]) - int(split[RIGHT_OPERAND])
             else:
-                caculation = split[LEFT_OPERAND] + split[RIGHT_OPERAND]
+                caculation = int(split[LEFT_OPERAND]) + int(split[RIGHT_OPERAND])
             caculation = str(caculation)
             while len(caculation) < len(split[RIGHT_OPERAND]):
                 caculation = ' ' + caculation
@@ -70,9 +70,7 @@ def arithmetic_arranger(problems, result = False):
     if result:
         for collum in standard_table:
             arranged_problems += collum[RESULT]
-        if collum == standard_table[-1]:
-            arranged_problems += '\n'
-        else:
+        if collum != standard_table[-1]:
             arranged_problems += ' ' * 4
 
     return arranged_problems
